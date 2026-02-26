1026fb70735a3dc1abaf49aae52ad3e5e428e57d
kandruva <k.vamsi5454@gmail.com>
2026-02-26 23:07:53 +0530

    changed bubble sort code to quick sort in Pro37.java fixed the registration page alignment

--- Pro37.java
+++ Pro37.java
@@ -1,9 +1,9 @@
 /*
-For the given set of numbers perform bubble sort and find the user entered number using linear and binary search.
-ar = {4,3,2,5,2,1,7,9,8};
+For the given set of numbers perform quick sort and find the user entered number using linear and binary search.
+ar = {7,3,2,5,1,6,4,9,8};
 O/P - 
 Enter the number to search: 4
-Sorted array: 1 2 3 4 5 6 7 8 9
+Sorted array: 1 2 3 4 5 6 7 9 8
 Found at index by linear search: 3
 Found at index by binary search: 3
 */
@@ -13,24 +13,14 @@ import java.util.Scanner;
 
 class FindNum
 {
-  int[] ar = {4,3,2,5,6,1,7,9,8};
+  int steps = 0;
+  int[] ar = {7,3,2,5,1,6,4,9,8};
   public void performSortAndFind()
   {
     Scanner scan = new Scanner(System.in);
     System.out.print("Enter the number to search: ");
     int num = scan.nextInt();
-    for(int i=0; i<ar.length; i++)
-    {
-      for(int j=0; j<ar.length-i-1; j++)
-      {
-        if(ar[j] > ar[j+1])
-        {
-          int temp= ar[j];
-          ar[j] = ar[j+1];
-          ar[j+1] = temp;
-        }
-      }
-    }
+    performQuickSort(0, ar.length-1);
     System.out.print("Sorted array: ");
     for(int i=0; i<ar.length; i++)
     {
@@ -67,6 +57,46 @@ class FindNum
     }
     if(l>r)
        System.out.println("Not found using binary search");   
+  }
+
+  public void performQuickSort(int l, int r)
+  {
+    int[] temp = new int[ar.length];
+    if(l==r || r<0 || l<0 || l>r)
+       return;
+    int pivot = (l+r)/2;
+    for(int i=0; i<l; i++)
+    {
+      temp[i] = ar[i];
+    }
+    int j=l;
+    for(int i=l; i<=r; i++)
+    {
+      if(ar[i] < ar[pivot])
+      {
+         temp[j] = ar[i];
+         j++;
+      }
+    }
+    int newp = j;
+    temp[j] = ar[pivot];
+    j++;
+    for(int i=l; i<=r; i++)
+    {
+      if(ar[i] > ar[pivot])
+      {
+         temp[j] = ar[i];
+         j++;
+      }
+    }
+    for(int i=(r+1); i<ar.length; i++)
+    {
+      temp[i] = ar[i];
+    }
+    for(int i=0; i<ar.length; i++)
+        ar[i] = temp[i];
+    performQuickSort(l, newp-1);
+    performQuickSort(newp+1, r);
   }
 }
 
